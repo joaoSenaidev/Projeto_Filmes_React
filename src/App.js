fe97048e2ba7d126dbc0dbd85ef04bbe401cf5f7
Joao Pedro <joaocruz.oliver17@gmail.com>
2025-04-24 08:39:41 -0300

    feat: adicionando pastas

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+/*import Login from './pages/login/Login.jsx'*/
+import CadastroFilme from './pages/cadastroFilme/CadastroFilme.jsx';
+//responsavel para criacao de telas
+function App() {
+  return (
+    <>
+   {/*chamar as paginas */} 
+   {/*<Login/>*/}
+    <CadastroFilme/>
+    </>
+  );
+}
+
+//responsavel para exportar um componente por arquivo
+export default App;
